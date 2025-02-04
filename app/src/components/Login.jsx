cda44808acfc310aa6b9f57f6a3b68daa8257bdd
dchen046 <dchen046@gmail.com>
2025-02-04 13:31:49 -0500

    finished log in page layout

---
+++ app/src/components/Login.jsx
@@ -0,0 +1,46 @@
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+
+const Login = () => {
+    return (
+        <div>
+            <p>Please Log In</p>
+            <LoginForm />
+        </div>
+
+    );
+}
+
+const LoginForm = () => {
+    return (
+        <div className=''>
+            <Form action='' method='POST'>
+                <FloatingLabel
+                    controlId="email"
+                    label="Email address"
+                    className="mb-3"
+                >
+                    <Form.Control type="email" placeholder="name@example.com" autoComplete='off' />
+                </FloatingLabel>
+
+                <FloatingLabel
+                    controlId="password"
+                    label="Password"
+                    className="mb-3"
+                >
+                    <Form.Control type="password" placeholder="password" />
+                </FloatingLabel>
+
+                <Button as="input" type="submit" value="Login" className='w-100' />
+            </Form>
+            <hr></hr>
+            <div>
+                <p> Don&apos;t Have An Account?</p>
+                <Button variant='secondary' href='/signup' value='Sign Up' className='w-100'>Sign Up</Button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css'
 import App from './App.jsx'
+import Login from './components/Login.jsx';
 
-createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/app',
+    element: <App />
+  }
+])
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
