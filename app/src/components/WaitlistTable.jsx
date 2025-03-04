83aecf3d93469a7495128b9bc2fc9fca33e1245f
dchen046 <dchen046@gmail.com>
2025-03-04 13:15:38 -0500

    fixed table positioning and added notification button

--- app/src/components/WaitlistTable.jsx
+++ app/src/components/WaitlistTable.jsx
@@ -1,32 +1,32 @@
 import Container from 'react-bootstrap/esm/Container';
 import Table from 'react-bootstrap/Table';
 import { useWaitlistContext } from '../context/WaitlistContext';
-import { MdDelete } from "react-icons/md";
+import { MdDelete, MdNotificationsActive } from "react-icons/md";
 import { FaEdit, FaCheck } from "react-icons/fa";
 import { Button } from 'react-bootstrap';
-
-
-
 
 const WaitlistTable = () => {
     return (
         <Container>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Size</th>
-                        <th>Time</th>
-                        <th>Notes</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {AddWaitlistRow()}
-                </tbody>
-            </Table>
+            <h3>Today&apos;s Reservations</h3>
+            <div className='d-flex justify-content-center'>
+                <Table striped bordered hover className='' size='sm'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Size</th>
+                            <th>Time</th>
+                            <th className='w-25'>Notes</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {AddWaitlistRow()}
+                    </tbody>
+                </Table>
+            </div>
         </Container>
     );
 }
@@ -40,20 +40,24 @@ const AddWaitlistRow = () => {
                 <tr key={entry.phone}>
                     <td>{index + 1}</td>
                     <td>{entry.name}</td>
-                    <td>{entry.phone}</td>
+                    <td>{entry.phone ? '123-123-1234' : '123'}</td>
                     <td>{entry.size}</td>
                     <td>{formatTime(entry.time)}</td>
                     <td>{entry.notes}</td>
                     <td>
-                        <Button variant='danger m-2' size='sm'>
-                            <MdDelete />
+                        <Button variant='warning' className='m-2' size='sm'>
+                            <MdNotificationsActive />
                         </Button>
 
-                        <Button variant='warning m-2' size='sm'>
+                        <Button variant='secondary' size='sm' className='m-2'>
                             <FaEdit />
                         </Button>
 
-                        <Button variant='success m-2' size='sm'>
+                        <Button variant='danger' size='sm' className='m-2'>
+                            <MdDelete />
+                        </Button>
+
+                        <Button variant='success' size='sm' className='m-2'>
                             <FaCheck />
                         </Button>
                     </td>
@@ -66,7 +70,8 @@ const AddWaitlistRow = () => {
 const formatTime = (datetime) => {
     const [date, time] = datetime.split('T');
     const [hours, mins] = time.split(':');
-    const formattedTime = `${date} ${hours}:${mins}`;
+    // const formattedTime = `${date} ${hours}:${mins}`;
+    const formattedTime = `${hours - 12}:${mins} ${hours >= 12 ? 'PM' : 'AM'}`;
     return formattedTime;
 }
 
