fb90b0d2b88a73c379d150f210ff89b578b366b8
dchen046 <dchen046@gmail.com>
2025-03-11 18:54:27 -0400

    added toggle states for tables on floorplan

--- app/src/components/Layout/Floorplan.jsx
+++ app/src/components/Layout/Floorplan.jsx
@@ -1,12 +1,8 @@
 import PropTypes from "prop-types";
-import { Button, Col, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Row } from "react-bootstrap";
 
-/**
- * Start:
- * Length:
- */
 const getTables = (grid) => {
-    console.log(grid.length);
     const visited = [];
     for (let row = 0; row < grid.length; ++row) {
         const newRow = []
@@ -25,8 +21,6 @@ const getTables = (grid) => {
                 continue;
             };
             let checkRow = row + 1;
-            console.log(checkRow);
-            console.log(col);
             let size = 0;
             while (checkRow < grid.length && grid[checkRow][col].used) {
                 visited[checkRow][col] = true;
@@ -39,48 +33,31 @@ const getTables = (grid) => {
         tables.push(colDetails);
     }
 
-
-    // for (let row = 0; row < grid.length; ++row) {
-    //     const rowDetails = [];
-    //     for (let col = 0; col < grid[0].length; ++col) {
-    //         if (visited[row][col] || !grid[row][col].used) continue;
-    //         let checkRow = row + 1;
-    //         let size = 1;
-    //         while (grid[checkRow][col].used) {
-    //             visited[checkRow][col] = true;
-    //             ++size;
-    //             ++checkRow;
-    //         }
-    //         const tableDetails = {
-    //             start: [row, col],
-    //             size: size
-    //         };
-    //         rowDetails.push(tableDetails);
-    //     }
-    //     tables.push(rowDetails);
-    // }
-
     return tables;
 }
 
 const Tables = ({ size }) => {
-
     const tableStyle = {
         minHeight: size == 0 ? '4rem' : `${size * 4}rem`,
         visibility: size == 0 ? 'hidden' : ''
     };
 
+    const [occupied, setOccupied] = useState(false);
+
+    const handleClick = () => {
+        setOccupied(prev => !prev);
+    }
+
     return (
-        <>
-            <Button
-                variant="info"
-                className="col-1 rounded-0 w-100"
-                size="lg"
-                disabled={size == 0}
-                style={tableStyle}
-            >
-            </Button>
-        </>
+        <Button
+            variant={occupied ? 'danger' : 'info'}
+            className="rounded-0 w-100"
+            size="lg"
+            disabled={size == 0}
+            style={tableStyle}
+            onClick={handleClick}
+        >
+        </Button>
     )
 }
 
@@ -93,25 +70,10 @@ const Floorplan = ({ grid }) => {
                 tables.map((col, rIndex) => {
                     return (
                         <div key={rIndex} className="col-1">
-                            {/* <Button>HI</Button>
-                             <Button>HI</Button>
-                             <Button>HI</Button>
-                            <Row>
-                                <Button>HI</Button>
-                            </Row>
-                            <Row>
-                                <Button>HI</Button>
-                            </Row>
-                            <Row>
-                                <Button>HI</Button>
-                            </Row> */}
                             {
                                 col.map((tableSize, index) => {
                                     return (
-                                        <div key={index}>
-                                            <Tables size={tableSize} />
-                                        </div>
-
+                                        <Tables key={index} size={tableSize} />
                                     )
                                 })
                             }
